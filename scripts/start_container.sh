d16df67d9fb0f1ee198fbd033306a96c06225a58
Karanam Vinodh <karanamvinodh@outlook.com>
2023-08-16 23:47:27 +0530

    updated files for start container script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push karanamvinodh/simple-python-app:latest
+docker pull karanamvinodh/simple-python-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 karanamvinodh/simple-python-app
